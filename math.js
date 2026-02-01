6e871543f365cec042bf50e2c86f1ec069ed2ddd
mkrafiul13453 <mkrafiul13453@gamil.com>
2026-02-02 00:36:52 +0600

    math is done

---
+++ math.js
@@ -0,0 +1,19 @@
+const min = Math.min(45,98,35,12,78,64,32,11);
+console.log(min);
+const max = Math.max(6,98,47,12,35,48,52,10,90);
+console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-5));
+console.log(Math.round(2.25));
+console.log(Math.round(2.85));
+console.log(Math.floor(2.95));
+console.log(Math.floor(2.99999995));
+console.log(Math.ceil(2.999995));
+console.log(Math.ceil(2.0000001));
+
+
+console.log(Math.random());
+console.log(Math.random()*10);
+const rand = Math.round(Math.random()*10);
+console.log(rand);
